a327cd63a07e5ee26eabca0d2aba6aa393128dec
Anil Yadav <anil.yadav@vtransgroup.com>
2026-04-29 15:59:13 +0530

    1.0.7 7

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -16,5 +16,5 @@ class AppConstants {
   static const double borderRadius32 = 32.0;
 
   static const String appName = 'PDF Master';
-  static const String version = 'v1.0.6 (build 6)';
+  static const String version = 'v1.0.7 (build 7)';
 }
